f87a6fd5ddd142831cf075526e1579c0332c22e5
Topher_MacbookPro <Christophercthomas@gmail.com>
2016-07-08 21:07:50 -0400

    styled code to PEP8, Also added Comments note: I do not know what a lot of the code does, I just know the outputs for both idl and python match. So for those comments I just copied and pasted what was on the idl file to the pyhton file

--- ConvertDraft.py
+++ ConvertDraft.py
@@ -1,31 +1,39 @@
 import sys
 from variables import *
 
-# TODO: Delete This when done
-# Checks the dec value of a hex.
-#i = int('48', 16)
-#print 'segments in the DSN minor frame counter, Target: ' + repr(i)
 
-# pro prb, olun,a,b,c,...,z,format=fmt
+# prb takes in two arguments, olun and param_array.
+# olun is the debug file to write to.
+# param_array is an array of strings that is combined into a single string.
+# prb takes a string and writes it to the terminal and a debug file.
 def prb(olun, param_array):
     str = ' '.join(param_array)
     print(str)
     olun.write(str + '\n')
 
+# Takes the first argument, which is the name of calabration data file,
+# and saves it to cal_data_file.
+cal_data_file = sys.argv[1]
+if cal_data_file == '':
+    print('ERROR: No Data File Given)
 
-# caldatafile: data file to call
-# view number: 0 or 1
-# acalimg_sfdu(caldatafile,view)
-data_file = sys.argv[1]  # caldatafile
-if data_file == '':
-    print('ERROR: -> data_file: ' + data_file)
+# TODO: Add implement code
+# data_file = sys.argv[2]  # View Number
 
+# Opens the Calibration Data File in binary mode.
+# ".read()" reads and saves the file's contents to the variable "il".
+il = open(data_file, 'r+b').read()
 
-f = open(data_file, 'r+b')
-il = f.read()
-f.close()
+r = []
+for i in range(len(il)/sfdurecl):
+    r.append(il[i*sfdurecl:i*sfdurecl+sfdurecl])
+
+# Opens debug file to be written to
 ol = open('../data2/test_file_python.txt', 'w')
 
+#
+# start processing the cal data files
+#
 
 prb(ol, ['ACA cal data file:', data_file])
 prb(ol, '')
@@ -34,11 +42,9 @@ prb(ol, ['Valid DSN record sync is:', str(VCDUsync), str(DSNch)])
 prb(ol, '')
 
 
-r = []
-for i in range(len(il)/sfdurecl):
-    r.append(il[i*sfdurecl:i*sfdurecl+sfdurecl])
-
-
+#
+# Inspect each SFDU in the file and check its contents
+#
 for rr in r:
     ir = []
     for rrr in rr:
@@ -49,55 +55,101 @@ for rr in r:
     notexplained = 1
 
     if (synctest1 == SFDUsync and synctest2 == VCDUsync):
+        # This SFDU starts with a correct sync pattern
         notexplained = 0
         v = v+1
         ch = ir[sfduhdrl+4:sfduhdrl+6]
         if ch[1] == DSNch[1]:
+            # the record is a "DSN record" from the ACA
 
+            # test if the DSN record contains only fill data
             testr = [i for i in ir[sfduhdrl+10:sfduhdrl+1029] if i != 170]
             testn = len(testr)
 
             if testn > 3:
                 idx.append(indexo)
-                count.append(ir[sfduhdrl+6]*65536+ir[sfduhdrl+7]*256+ir[sfduhdrl + 8])
+                # TODO: FIgure out how to wrap line - Christopher Thomas
+                count.append(ir[sfduhdrl + 6] * 65536 + ir[sfduhdrl + 7] * 256 + ir[sfduhdrl + 8])
+
+                # I will carry time since the start of 1998 (the Chandra FITS file reference)
+                # which is 14610 days after the start of 1958 (the ERT reference), in seconds.
+                # NOTE: I ignore the 32.182 second + leap seconds offset between UTC (for ERT) and TT (for FITS)
+                # TODO: FIgure out how to wrap line - Christopher Thomas
                 ert.append((ir[42]*256 + ir[43] - 14610) * 84600e0 + (((ir[44] * 256 + ir[45])*256 + ir[46])*256 + ir[47])/1e3 + (ir[48]*256 + ir[49])/1e6)
                 tdsn = tdsn+1
 
             else:
-                tfil=tfil+1
+                # DSN minor frame contains only fill data
+                tfil = tfil+1
 
-        if ch!=DSNch:
-            tother=tother+1
-            prb(ol,['record',`i+1`,' has unknown VCID: ',' '.join(str(x) for x in ir[sfduhdrl:sfduhdrl+6])])
+        if ch != DSNch:
+            tother = tother+1
+            prb(ol, ['record', repr(i+1), ' has unknown VCID: ',
+                ' '.join(str(x) for x in ir[sfduhdrl:sfduhdrl+6])])
 
-    indexo=indexo+1
-    if notexplained==1:
-        prb(ol,[' *** record ',`i`, 'start is weird: ',' '.join(ir[0:sfduhdrl+12])])
+    indexo = indexo+1
+    if notexplained == 1:
+        # I don't understand the start of the 1029-byte record at all!
+        prb(ol, [' *** record ', repr(i), 'start is weird: ',
+            ' '.join(ir[0:sfduhdrl+12])])
 
-#End of Loop
+# End of While Loop
 
 
-prb(ol,'')
-prb(ol,[`tdsn`,' DSN minor frames with pixel data in the file'])
-prb(ol,[`tfil`,' FIL minor frames in the file'])
-prb(ol,[`tother`,' OTHER minor frames in the file'])
-prb(ol,'')
-prb(ol,[`indexo`,' SFDUs in the file'])
-prb(ol,'')
+prb(ol, '')
+prb(ol, [repr(tdsn), ' DSN minor frames with pixel data in the file'])
+prb(ol, [repr(tfil), ' FIL minor frames in the file'])
+prb(ol, [repr(tother), ' OTHER minor frames in the file'])
+prb(ol, '')
+prb(ol, [repr(indexo), ' SFDUs in the file'])
+prb(ol, '')
 
 if tdsn <= 0:
-    prb(ol,['No DSN minor frames in the file, Bozo! I''m stopping!'])
+    prb(ol, ['No DSN minor frames in the file, Bozo! I''m stopping!'])
 
 idx = idx[0:tdsn]
 count = count[0:tdsn]
 ert = ert[0:tdsn]
 
-#disc=where((count-shift(count,1)) ne 1,ndisc)
-disc = [i for i in (count - np.roll(count,1)) if i != 1]
+#
+# TERMINOLOGY: a "segment" is where the 24-bit DSN record counter is contiguous
+# (i.e. increments by 1 between DSN records)
+# extract the DSN record counter segments
+# disc contains the first indices of new DSN segments
+#
+disc = [i for i in (count - np.roll(count, 1)) if i != 1]
 ndisc = len(disc)
-disc = [disc,tdsn]
+disc = [disc, tdsn]
 
-prb(ol,[`ndisc`,'segments in the DSN minor frame counter'])
+prb(ol, [repr(ndisc), 'segments in the DSN minor frame counter'])
 
 
+#
+# process each segment (i.e. contiguous set) of DSN records
+#
+"""
+for f in range(ndisc):
+    prb(ol, '')
+    p drb(ol, ['>>>>>>>>>>>>>>> Processing DSN record segment ', repr(f),
+        ' <<<<<<<<<<<<<<<'])
+    prb(ol, '')
+
+    ndrec = disc[f+1]-disc(1)
+    maindat = [[], []]
+    m = 0
+    for j = disc(f) in range(disc(f+1)):  # disc(f+1)-1
+        k = idx(j)
+        ir1 = r(k)
+        # maindat(*,m)=ir1(sfduhdrl+9:sfduhdrl+1028)
+        mainddat.append(ir1[sfduhdrl+9: sfduhdrl+1029])
+        m=m+1
+    scount = count[disc[f]:disc(f+1)]
+    sert = ert[disc[f]:disc(f+1)]
+    prb(ol, ['first, last, delta in DSN minor frame counter:  ',
+        repr(scount(0)), '  ', repr(scount(ndrec-1)), '  ',
+        repr(scount(ndrec-1)-scount(0)+1)])
+    prb(ol, ['DSN secondary header range: ', min(maindat(0,*)),
+        max(maindat(0,*))]) # Have Fun!
+    prb(ol, '')
+"""
 ol.close()
--- variables.py
+++ variables.py
@@ -1,88 +1,81 @@
 import numpy as np
 
-#get_lun,il
-#NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
-#get_lun,ol
-#NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
-
-#SFDUsync = byte('NJPL')
+# SFDUsync = byte('NJPL')
 SFDUsync = [78,  74,  80,  76]
-#VCDUsync = ['1A'xb,'CF'xb,'FC'xb,'1D'xb]
-VCDUsync = [26,207,252,29]
-#DSNch = ['41'xb,'93'xb]
-DSNch = [65,147]
+# VCDUsync = ['1A'xb,'CF'xb,'FC'xb,'1D'xb]
+VCDUsync = [26, 207, 252, 29]
+# DSNch = ['41'xb,'93'xb]
+DSNch = [65, 147]
 
 
-#sfdurecl = 1134  ; Number of bytes in an SFDU
+# Number of bytes in an SFDU
 sfdurecl = 1134
-#sfduhdrl = 104   ; Number of bytes in SFDU header
+# Number of bytes in SFDU header
 sfduhdrl = 104
-
-#dsnrecl = 1029   ; Number of bytes per truncated DSN record
+# Number of bytes per truncated DSN record
 dsnrecl = 1029
-
-#calrecl = 1040   ; Number of bytes per cal record
+# Number of bytes per cal record
 calrecl = 1040
 
 
-#cltick = 2d-6    ; 2 microsecond clock period
-cltick=2e-6
-#TODO: FIGURE OUT WHAT '2d' IS
+# 2 microsecond clock period
+cltick = 2e-6
 
-#quadstr=['A','C','B','D']    ; quadrant identifiers for values of QUADID
-quadstr = ['A','C','B','D']
+# quadrant identifiers for values of QUADID
+quadstr = ['A', 'C', 'B', 'D']
 
 
-#sl0 ={ACAL0,TIME_ERT:0.0d0,REC_CTR:0L,ADC_BIAS:0,INTEG:0.0,CALTYPE:0,REPLICA:0,$
-#            COLNEG:0,ROW:0,SCI_HDR_CTR:0,TWO_US_CLK:0L,PIXDATA:intarr(512)}
+#
+# create a structure record of the L0 product type
+#
+
 sl0 = {
-    'TIME_ERT':0.0,
-    'REC_CTR':0L,
-    'ADC_BIAS':0,
-    'INTEG':0.0,
-    'CALTYPE':0,
-    'REPLICA':0,
-    'COLNEG':0,
-    'ROW':0,
-    'SCI_HDR_CTR':0,
-    'TWO_US_CLK':0L,
-    'PIXDATA':[None]*512
+    'TIME_ERT': 0.0,
+    'REC_CTR': 0L,
+    'ADC_BIAS': 0,
+    'INTEG': 0.0,
+    'CALTYPE': 0,
+    'REPLICA': 0,
+    'COLNEG': 0,
+    'ROW': 0,
+    'SCI_HDR_CTR': 0,
+    'TWO_US_CLK': 0L,
+    'PIXDATA': [None] * 512
 }
 
-#sl0n=0
-sl0n=0
+# sl0n=0
+sl0n = 0
 
-#i=0L
-indexo=0 # Named indexo so it is not confused with "i" or "index"
-#v=0L
-v=0
-#tdsn=0L
-tdsn=0
-#tfil=0L
-tfil=0
-#tother=0L
-tother=0
-#nrecmax = 20000
-nrecmax=20000
-#idx=lonarr(nrecmax)
+#
+# count the number of records in the file
+#
+
+# i=0L
+indexo = 0  # Named indexo so it is not confused with "i" or "index"
+v = 0
+tdsn = 0
+tfil = 0
+tother = 0
+nrecmax = 20000
+# idx=lonarr(nrecmax)
 idx = []
-#count=lonarr(nrecmax)
+# count=lonarr(nrecmax)
 count = []
-#ert=dblarr(nrecmax)
+# ert=dblarr(nrecmax)
 ert = []
-#timcount=0 ; a counter for composite total images written to FITS files
-timecount=0
-#rotally=intarr(1024,2)
-#NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
-#rofill=intarr(1024,2)
-#NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
-#imarr=fltarr(1024,1024)
-#NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
-#quadarr=fltarr(512,512)
-#NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
-#quadfill=intarr(512)
-#NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
-#quadtally=intarr(4)
-#NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
-#prevquadid=-1
-prevquadid=-1
+#  a counter for composite total images written to FITS files
+timecount = 0
+# rotally=intarr(1024,2)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# rofill=intarr(1024,2)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# imarr=fltarr(1024,1024)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# quadarr=fltarr(512,512)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# quadfill=intarr(512)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# quadtally=intarr(4)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# prevquadid=-1
+prevquadid =- 1

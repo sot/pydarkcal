40439f17ff24ba7c1624fe1c3284a184dda2be0c
Topher_MacbookPro <Christophercthomas@gmail.com>
2016-07-28 16:00:33 -0400

    variable disc problems, Jul28

--- ConvertDraft.py
+++ ConvertDraft.py
@@ -1,5 +1,89 @@
 import sys
-from variables import *
+import numpy as np
+
+# ======================
+# Initializing Variables
+# ======================
+
+# SFDUsync = byte('NJPL')
+SFDUsync = [78,  74,  80,  76]
+# VCDUsync = ['1A'xb,'CF'xb,'FC'xb,'1D'xb]
+VCDUsync = [26, 207, 252, 29]
+# DSNch = ['41'xb,'93'xb]
+DSNch = [65, 147]
+
+
+# Number of bytes in an SFDU
+sfdurecl = 1134
+# Number of bytes in SFDU header
+sfduhdrl = 104
+# Number of bytes per truncated DSN record
+dsnrecl = 1029
+# Number of bytes per cal record
+calrecl = 1040
+
+
+# 2 microsecond clock period
+cltick = 2e-6
+
+# quadrant identifiers for values of QUADID
+quadstr = ['A', 'C', 'B', 'D']
+
+
+#
+# create a structure record of the L0 product type
+#
+
+sl0 = {
+    'TIME_ERT': 0.0,
+    'REC_CTR': 0L,
+    'ADC_BIAS': 0,
+    'INTEG': 0.0,
+    'CALTYPE': 0,
+    'REPLICA': 0,
+    'COLNEG': 0,
+    'ROW': 0,
+    'SCI_HDR_CTR': 0,
+    'TWO_US_CLK': 0L,
+    'PIXDATA': [None] * 512
+}
+
+# sl0n=0
+sl0n = 0
+
+#
+# count the number of records in the file
+#
+
+# i=0L
+i = 0
+v = 0
+tdsn = 0
+tfil = 0
+tother = 0
+nrecmax = 20000
+# idx=lonarr(nrecmax)
+idx = []
+# count=lonarr(nrecmax)
+count = []
+# ert=dblarr(nrecmax)
+ert = []
+#  a counter for composite total images written to FITS files
+timecount = 0
+# rotally=intarr(1024,2)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# rofill=intarr(1024,2)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# imarr=fltarr(1024,1024)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# quadarr=fltarr(512,512)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# quadfill=intarr(512)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# quadtally=intarr(4)
+# NOTE: DOES NOT NEED TO BE INITIALIZED AT THIS TIME
+# prevquadid=-1
+# NOTE: prevquadid = -1
 
 
 # prb takes in two arguments, olun and param_array.
@@ -24,11 +108,6 @@ if cal_data_file == '':
 # ".read()" reads and saves the file's contents to the variable "il".
 il = open(cal_data_file, 'r+b').read()
 
-r = []
-for i in range(len(il)/sfdurecl):
-    # TODO: Bring for loop here
-    r.append(il[i*sfdurecl:i*sfdurecl+sfdurecl])
-
 # Opens debug file to be written to
 ol = open('../data2/test_file_python.txt', 'w')
 
@@ -46,11 +125,19 @@ prb(ol, '')
 #
 # Inspect each SFDU in the file and check its contents
 #
-for rr in r:
-    ir = []
-    for rrr in rr:
-        ir.append(ord(rrr))
 
+# Looping through cal_data_file in steps of sfdurecl (1134)
+for indexo in range(len(il)/sfdurecl):
+
+    # Setting current step of 1134 to "r"
+    r = il[indexo*sfdurecl:indexo*sfdurecl+sfdurecl]
+
+    # Converting step "r" or decimals, saving to "ir"
+    ir = []
+    for rr in r:
+        ir.append(ord(rr))
+
+    # Testing synchronization
     synctest1 = ir[0:4]
     synctest2 = ir[sfduhdrl:sfduhdrl+4]
     notexplained = 1
@@ -61,15 +148,19 @@ for rr in r:
         v = v+1
         ch = ir[sfduhdrl+4:sfduhdrl+6]
         if ch[1] == DSNch[1]:
-            # the record is a "DSN record" from the ACA
 
             # test if the DSN record contains only fill data
+            # "testr" is all elements of "ir" that are not equal to 170
             testr = [i for i in ir[sfduhdrl+10:sfduhdrl+1029] if i != 170]
             testn = len(testr)
 
             if testn > 3:
+
+                # idx appending integer indexo to list
                 idx.append(indexo)
-                # TODO: FIgure out how to wrap line - Christopher Thomas
+
+                # No Idea what the software is supose to be doing as a hole
+                # No Usefull comment here, sorry - Christopher Thomas
                 count.append(ir[sfduhdrl + 6] * 65536 + ir[sfduhdrl + 7] *
                              256 + ir[sfduhdrl + 8])
 
@@ -81,38 +172,39 @@ for rr in r:
                 ert.append((ir[42]*256 + ir[43] - 14610) * 84600e0 +
                            (((ir[44] * 256 + ir[45])*256 + ir[46]) * 256 +
                            ir[47]) / 1e3 + (ir[48] * 256 + ir[49])/1e6)
+
                 tdsn = tdsn+1
 
             else:
                 # DSN minor frame contains only fill data
                 tfil = tfil+1
 
-        if ch != DSNch:
+        if ch != DSNch:  # If this is hit, something bad happended...
             tother = tother+1
             prb(ol, ['record', repr(i+1), ' has unknown VCID: ',
                 ' '.join(str(x) for x in ir[sfduhdrl:sfduhdrl+6])])
 
-    indexo = indexo+1
-    if notexplained == 1:
-        # I don't understand the start of the 1029-byte record at all!
+    if notexplained == 1:  # If this is hit, something bad happended...
+        # program does not understand the start of the 1029-byte record at all!
         prb(ol, [' *** record ', repr(i), 'start is weird: ',
             ' '.join(ir[0:sfduhdrl+12])])
 
 # End of While Loop
 
-
 prb(ol, '')
-prb(ol, [repr(tdsn), ' DSN minor frames with pixel data in the file'])
-prb(ol, [repr(tfil), ' FIL minor frames in the file'])
-prb(ol, [repr(tother), ' OTHER minor frames in the file'])
+prb(ol, [repr(tdsn), ' DSN minor frames with pixel data in the file'])  # 4285
+prb(ol, [repr(tfil), ' FIL minor frames in the file'])  # 29015
+prb(ol, [repr(tother), ' TOTHER minor frames in the file'])  # 0
 prb(ol, '')
-prb(ol, [repr(indexo), ' SFDUs in the file'])
+prb(ol, [repr(indexo), ' SFDUs in the file'])  # 33273
 prb(ol, '')
 
 
 if tdsn <= 0:
     prb(ol, ['No DSN minor frames in the file, Bozo! I''m stopping!'])
 
+# TBH I think this may be useless in python version, Needs to be tested to
+# see if idx,count, & ert change before and after these three lines
 idx = idx[0:tdsn]
 count = count[0:tdsn]
 ert = ert[0:tdsn]
@@ -123,39 +215,138 @@ ert = ert[0:tdsn]
 # extract the DSN record counter segments
 # disc contains the first indices of new DSN segments
 #
+
+# ==========================================================================
+# ==========================================================================
+# ==========================================================================
+# ==============================JEAN LOOK HERE==============================
+# ==========================================================================
+# ==========================================================================
+# ==========================================================================
+# disc equals count - shifted_count at all elements not equals to 1
 disc = [i for i in (count - np.roll(count, 1)) if i != 1]
+# ndisc equals length of disc, NOTE: ndisc is corrent length.
 ndisc = len(disc)
+# disc equals disc appened tdsn (4258)
 disc = [disc, tdsn]
 
-prb(ol, [repr(ndisc), 'segments in the DSN minor frame counter'])
+# PROBLEM: disc in IDL vs Python are not equal.
+# IDL
+# 0          62         124         184         245         306
+# 367         429         490         532         593         653
+# 714         774         835         897         958        1020
+# 1063        1125        1185        1246        1308        1369
+# 1430        1493        1553        1596        1657        1719
+# 1781        1842        1905        1965        2025        2087
+# 2129        2191        2252        2312        2373        2435
+# 2497        2558        2620        2661        2722        2784
+# 2845        2908        2969        3029        3091        3151
+# 3193        3254        3314        3375        3437        3499
+# 3560        3623        3684        3726        3787        3849
+# 3910        3972        4033        4093        4155        4216
+
+# PYTHON
+# [-28586, 4, 4, 4, 5, 3, 4, 3, 4, 1559, 5, 5, 4, 4, 4, 3, 4, 3, 2262,
+# 4, 4, 4, 3, 4, 3, 4, 5, 2261, 4, 3, 3, 3, 4, 5, 4, 3, 2902, 5, 4, 4,
+# 4, 3, 3, 4, 3, 5080, 4, 3, 3, 4, 4, 4, 4, 4, 5080, 4, 4, 4, 3, 3, 3,
+# 3, 5, 5014, 4, 3, 3, 5, 4, 4, 4, 3]
+
+# Target is 72, current version hits target
+prb(ol, [repr(ndisc), 'segments in the DSN minor frame counter'])  # 72
 
 
+# =============
+# STOPPING HERE
+print "STOPPING\nsys.exit(0)"
+sys.exit(0)
+# STOPPING HERE
+# =============
+
+
+# TODO: Add more/personalized comments to code past this point.
+# Comment what I am doing and not just carry over comments from IDL
+
 #
-# process each segment (i.e. contiguous set) of DSN records
+# process each segment (i.e. contiguous set) of DSN records - RC
 #
-"""
 for f in range(ndisc):
     prb(ol, '')
-    p drb(ol, ['>>>>>>>>>>>>>>> Processing DSN record segment ', repr(f),
-         ' <<<<<<<<<<<<<<<'])
+    prb(ol, ['>>>>>>>>>>>>>>> Processing DSN record segment ', repr(f+1),
+        ' <<<<<<<<<<<<<<<'])
     prb(ol, '')
 
-    ndrec = disc[f+1]-disc(1)
+    ndrec = disc[f+1]-disc[1]
     maindat = [[], []]
     m = 0
-    for j = disc(f) in range(disc(f+1)):  # disc(f+1)-1
-        k = idx(j)
-        ir1 = r(k)
-        # maindat(*,m)=ir1(sfduhdrl+9:sfduhdrl+1028)
+    # for j=disc(f),disc(f+1)-1 do begin IDL
+    # for j = disc(f) in range(disc(f+1)): PYTHON BROKEN
+    print disc
+    print "****"
+    print disc[f]
+    print "****"
+    print disc[f+1]
+    for j in range(disc[f], disc[f+1]-1):  # PYTHON FIXED?
+        k = idx[j]
+        ir1 = r[k]
+        # maindat(*,m)=ir1(sfduhdrl+9:sfduhdrl+1028) IDL CODE
         mainddat.append(ir1[sfduhdrl+9: sfduhdrl+1029])
-        m=m+1
-    scount = count[disc[f]:disc(f+1)]
-    sert = ert[disc[f]:disc(f+1)]
+        m = m + 1
+    scount = count[disc[f]:disc[f+1]]
+    sert = ert[disc[f]:disc[f+1]]
     prb(ol, ['first, last, delta in DSN minor frame counter:  ',
-       repr(scount(0)), '  ', repr(scount(ndrec-1)), '  ',
-       repr(scount(ndrec-1)-scount(0)+1)])
-    prb(ol, ['DSN secondary header range: ', min(maindat(0,*)),
-       max(maindat(0,*))]) # Have Fun!
+        repr(scount[0]), '  ', repr(scount[ndrec-1]), '  ',
+        repr(scount[ndrec-1]-scount[0]+1)])
+    prb(ol, ['DSN secondary header range: ', np.min(np.array(maindat)[:, 0]),
+        np.max(np.array(maindat)[:, 0])])
     prb(ol, '')
-"""
+
+#
+# find the calibration data in the DSN records,
+# which is differently placed in alternating DSN records.
+# Test for both ways of packing.
+# TODO: Check array Index, Add 1 when needed
+    if ndrec == 1:
+        prb(ol, ['This is a single non-fill cal minor frame at record index ',
+                 repr(idx(disc(f))+1)])
+        prb(ol, "")
+        prb(ol, ["Error: ndrec does not equals 1 -> ndrec = ", repr(ndrec)])
+        # TODO: goto nextdisc
+    ev = [i for i in scount if (scount/2)*2 == scount]
+    nev = len(ev)
+    od = [i for i in scount if (scount/2)*2 != scount]
+    nod = len(od)
+    # Line 212 in acalimg.sfdu.pro
+    if not (maindat[1][ev] - 205 == 0 and maindat[1019][od] - 171 == 0):
+        calsync = 0
+    elif not (maindat[1][od] - 205 == 0 and maindat[1019][ev] - 171 == 0):
+        calsync = 1
+    else:
+        prb(ol, ["Cal data doesn't sync to DSN minor frame counter! Using",
+                 "previous sync..."])
+
+    if calsync == 0:
+        prb(ol, ['Cal records are synchronized to even DSN minor frame'
+                 'counter'])
+        for i in range(1017):
+            maindat[i][ev] = maindat[i+2][ev]
+        for i in range(1017):
+            maindat[i][od] = maindat[i+1][od]
+    elif calsync == 1:
+        prb(ol, ['Cal records are synchronized to odd DSN minor frame'
+                 'counter'])
+        for i in range(1017):
+            maindat[i][od] = maindat[i+2][od]
+        for i in range(1017):
+            maindat[i][ev] = maindat[i+1][ev]
+    # maindat = maindat[:1018][]
+    # maindat=reform(maindat,n_elements(maindat))
+    maindat.np.flatten()
+
+#
+# locate complete calibration records in the extracted cal data
+# and check constancy of cal record size
+#
+
+# acalimg_sfdu.pro LINE 227
+
 ol.close()
